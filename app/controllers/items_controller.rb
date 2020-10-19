825f49e57e3798d9af10483a12206c1d8d56b2a4
matyareo-matyarei <masa.ahonya.yuka.ahonya@gmail.com>
2020-10-20 05:04:29 +0900

    出品機能作業(1)
    出品ページの遷移、
    新規出品ボタン、
    ストロングパラメーターセット

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -7,5 +7,13 @@ class ItemsController < ApplicationController
   end
 
   def create
+    @item = Item.create(item_params)
   end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:imaage, :name, :descriptions, :category_id, :status_id, :burden_id, :area_id, :date_id, :price).merge(user_id: current_user.id)
+  end
+
 end
