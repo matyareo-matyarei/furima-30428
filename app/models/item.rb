329e30dcc4422508ef31c5356506e8cc7cb26aec
matyareo-matyarei <masa.ahonya.yuka.ahonya@gmail.com>
2020-10-19 21:05:51 +0900

    itemカラムのバリデーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,15 @@
 class Item < ApplicationRecord
   belongs_to :user
-  
+  has_one_attached :image
+  validates :image, presence: true
+  with_options presence: true do
+    validates :name
+    validates :descriptions
+    validates :category_id
+    validates :status_id
+    validates :burden_id
+    validates :area_id
+    validates :date_id
+    validates :price
+  end
 end
