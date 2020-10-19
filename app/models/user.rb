fc98cf3acccd4233cf2f59d188fe5f60752e8e50
matyareo-matyarei <masa.ahonya.yuka.ahonya@gmail.com>
2020-10-19 16:27:16 +0900

    Userモデルの単体テストコード
    モデルのバリデーションも書き直しをした。

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,18 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'Include both letters and numbers'
 
   validates :nickname, presence: true
+  validates :birthday, presence: true
 
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: 'Full-width characters' } do
+    validates :family_name
+    validates :first_name
+  end
+  with_options presence: true, format: { with: /\A[ァ-ン]+\z/, message: 'Full-width katakana characters' } do
+    validates :family_kana
+    validates :first_kana
+  end
 end
---
+++ spec/factories/users.rb
@@ -0,0 +1,14 @@
+FactoryBot.define do
+  factory :user do
+    nickname               {Faker::Name.name}
+    email                  {Faker::Internet.free_email}
+    password = Faker::Internet.password(min_length: 6)
+    password               {password}
+    password_confirmation  {password}
+    family_name            {"山田"}
+    first_name             {"太郎"}
+    family_kana            {"ヤマダ"}
+    first_kana             {"タロウ"}
+    birthday               {"1930-02-02"}
+  end
+end
