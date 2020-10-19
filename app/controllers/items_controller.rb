ec14e47dc04079dbe8d40b8301060f5d31a18bde
matyareo-matyarei <masa.ahonya.yuka.ahonya@gmail.com>
2020-10-20 06:49:53 +0900

    出品機能作業(2)
    出品できる時/できない時の条件分岐
    エラーハンドリング設定
    Active Hash導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,6 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
+
   def index
   end
 
@@ -7,7 +9,12 @@ class ItemsController < ApplicationController
   end
 
   def create
-    @item = Item.create(item_params)
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   private
