af5ecb9357f338fd3ee1a20f521e524c4ff77e51
matyareo-matyarei <masa.ahonya.yuka.ahonya@gmail.com>
2020-10-20 09:06:18 +0900

    出品機能作業(4)
    余計なコメント削除
    ActiveHashを利用するモデルの内容記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,10 +6,8 @@ class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :category, :status, :burden, :area, :schedule
 
-  #空の投稿を保存できないようにする
   validates :name, :descriptions, :category, :status, :burden, :area, :schedule, :price, presence: true
 
-  #ジャンルの選択が「--」の時は保存できないようにする(genre_idのid:1以外が保存できる)
   validates :category_id, numericality: { other_than: 1 } 
   validates :status_id, numericality: { other_than: 1 } 
   validates :burden_id, numericality: { other_than: 1 } 
