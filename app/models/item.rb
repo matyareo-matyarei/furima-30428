9d7fc428535b92d232d040e12fc2426389f6260c
matyareo-matyarei <masa.ahonya.yuka.ahonya@gmail.com>
2020-10-24 08:11:02 +0900

    orderモデルのアソシエーション修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,7 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
-  has_many :orders
+  has_one :order
 
   validates :image, presence: true
   validates :price, numericality: { message: 'Half-width number' }
