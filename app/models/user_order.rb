c9c13a59e7a1e1e63de9d51f13e4e4997606d12a
matyareo-matyarei <masa.ahonya.yuka.ahonya@gmail.com>
2020-10-23 09:11:49 +0900

    バリデーション記述修正＆addressesテーブル作成

--- app/models/user_order.rb
+++ app/models/user_order.rb
@@ -1,16 +1,25 @@
 class UserOder
   include ActiveModel::Model
-  attr_accessor :number, :postal_code, :prefecture, :city, :addresses, :building, :phone_number
+  attr_accessor :postal_code, :prefecture, :city, :addresses, :building, :phone_number
 
   with_options presence: true do
-    validates :nickname
-    validates :birthday
-    validates :family_name, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: 'Full-width characters' }
-    validates :first_name, format:  { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: 'Full-width characters' }
-    validates :family_kana, format: { with: /\A[ァ-ン]+\z/, message: 'Full-width katakana characters' }
-    validates :first_kana, format:  { with: /\A[ァ-ン]+\z/, message: 'Full-width katakana characters' }
+    
+    validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :prefecture, numericality: { other_than: 0, message: "Select" }
+    validates :city
+    validates :addresses
+    validates :phone_number, format: {with: /\A^[0-9]+$\z/, message: "Input only number"}
   end
-  
+
+  def save  # フォームから送られてきたパラメーター情報をテーブルに保存する処理
+    # ユーザーの情報を保存し、「user」という変数に入れている
+    user = User.create(name: name, name_reading: name_reading, nickname: nickname)
+    # 住所の情報を保存
+    Address.create(postal_code: postal_code, prefecture: prefecture, city: city, house_number: house_number, building_name: building_name,user_id: user.id)
+    # 寄付金の情報を保存
+    Donation.create(price: price, user_id: user.id)
+  end
+
 
 
 end
