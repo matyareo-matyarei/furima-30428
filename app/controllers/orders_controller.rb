3514aa57ed6a6d2007643b6ed6a5033d385b1c95
matyareo-matyarei <masa.ahonya.yuka.ahonya@gmail.com>
2020-10-23 17:14:43 +0900

    リファクタリング

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -14,17 +14,7 @@ class OrdersController < ApplicationController
     @item_order = ItemOrder.new(order_params)
     @item = Item.find(params[:item_id])
     if @item_order.valid?
-      binding.pry
-      Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
-      Payjp::Charge.create(
-        amount: @item[:price],  # 商品の値段
-        card: order_params[:token],    # カードトークン
-        currency: 'jpy'                 # 通貨の種類（日本円）
-      )
-
-
-
-
+      pay_item
       @item_order.save
       return redirect_to root_path
     else
@@ -36,7 +26,14 @@ class OrdersController < ApplicationController
   
   def order_params
     params.require(:item_order).permit(:postal_code, :prefecture_id, :city, :addresses, :building, :phone_number).merge(item_id: params[:item_id], user_id: current_user.id, token: params[:token])
-
   end
 
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: @item[:price],         # 商品の値段
+      card: order_params[:token],    # カードトークン
+      currency: 'jpy'                # 通貨の種類（日本円）
+    )
+  end
 end
