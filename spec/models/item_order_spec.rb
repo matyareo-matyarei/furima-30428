976c733945eccf181995ed3113cb9fbb8491c2d7
matyareo-matyarei <masa.ahonya.yuka.ahonya@gmail.com>
2020-10-24 18:43:55 +0900

    contextを用いて正常系と異常系に分けてテストコード記述

--- spec/models/item_order_spec.rb
+++ spec/models/item_order_spec.rb
@@ -5,66 +5,73 @@ RSpec.describe ItemOrder, type: :model do
     @item_order = FactoryBot.build(:item_order)
   end
 
-  it 'postal_codeとprefecture_idとcityとaddressesとbuildingとphone_numberとtokenとnumberとexp_monthととexp_yearとcvcとtokenがあれば保存ができること' do
-    expect(@item_order).to be_valid
-  end
-
-  it 'postal_codeが空では登録できないこと' do
-    @item_order.postal_code = nil
-    @item_order.valid?
-    expect(@item_order.errors.full_messages).to include("Postal code can't be blank")
-  end
-
-  it 'postal_codeには-がないと登録できない' do
-    @item_order.postal_code = '1234567'
-    @item_order.valid?
-    expect(@item_order.errors.full_messages).to include('Postal code is invalid. Include hyphen(-)')
-  end
-
-  it 'prefecture_idが選択されていないと登録できない' do
-    @item_order.prefecture_id = 1
-    @item_order.valid?
-    expect(@item_order.errors.full_messages).to include('Prefecture Select')
-  end
-
-  it 'cityが空では登録できない' do
-    @item_order.city = nil
-    @item_order.valid?
-    expect(@item_order.errors.full_messages).to include("City can't be blank")
-  end
-
-  it 'addressesが空では登録できない' do
-    @item_order.addresses = nil
-    @item_order.valid?
-    expect(@item_order.errors.full_messages).to include("Addresses can't be blank")
-  end
-
-  it 'buildingは空でも登録できる' do
-    @item_order.building = nil
-    expect(@item_order).to be_valid
-  end
+  describe '購入履歴の保存' do
+    context "購入情報が保存できる場合" do
+      it 'postal_codeとprefecture_idとcityとaddressesとbuildingとphone_numberとtokenとnumberとexp_monthととexp_yearとcvcとtokenがあれば保存ができること' do
+        expect(@item_order).to be_valid
+      end
   
-  it 'phone_numberが空では登録できない' do
-    @item_order.phone_number = nil
-    @item_order.valid?
-    expect(@item_order.errors.full_messages).to include("Phone number can't be blank")
-  end
-
-  it 'phone_numberに-があると登録できない' do
-    @item_order.phone_number = '03-123-4567'
-    @item_order.valid?
-    expect(@item_order.errors.full_messages).to include('Phone number Input only number')
-  end
-
-  it 'phone_numberは11桁以内でないと登録できない' do
-    @item_order.phone_number = '012345678901'
-    @item_order.valid?
-    expect(@item_order.errors.full_messages).to include('Phone number Input only number')
-  end
-
-  it 'tokenが空では登録できない' do
-    @item_order.token = nil
-    @item_order.valid?
-    expect(@item_order.errors.full_messages).to include("Token can't be blank")
+      it 'buildingは空でも登録できる' do
+        @item_order.building = nil
+        expect(@item_order).to be_valid
+      end
+    end
+  
+    context "購入情報が保存できない場合" do
+      it 'postal_codeが空では登録できないこと' do
+        @item_order.postal_code = nil
+        @item_order.valid?
+        expect(@item_order.errors.full_messages).to include("Postal code can't be blank")
+      end
+    
+      it 'postal_codeには-がないと登録できない' do
+        @item_order.postal_code = '1234567'
+        @item_order.valid?
+        expect(@item_order.errors.full_messages).to include('Postal code is invalid. Include hyphen(-)')
+      end
+    
+      it 'prefecture_idが選択されていないと登録できない' do
+        @item_order.prefecture_id = 1
+        @item_order.valid?
+        expect(@item_order.errors.full_messages).to include('Prefecture Select')
+      end
+    
+      it 'cityが空では登録できない' do
+        @item_order.city = nil
+        @item_order.valid?
+        expect(@item_order.errors.full_messages).to include("City can't be blank")
+      end
+    
+      it 'addressesが空では登録できない' do
+        @item_order.addresses = nil
+        @item_order.valid?
+        expect(@item_order.errors.full_messages).to include("Addresses can't be blank")
+      end
+    
+      
+      it 'phone_numberが空では登録できない' do
+        @item_order.phone_number = nil
+        @item_order.valid?
+        expect(@item_order.errors.full_messages).to include("Phone number can't be blank")
+      end
+    
+      it 'phone_numberに-があると登録できない' do
+        @item_order.phone_number = '03-123-4567'
+        @item_order.valid?
+        expect(@item_order.errors.full_messages).to include('Phone number Input only number')
+      end
+    
+      it 'phone_numberは11桁以内でないと登録できない' do
+        @item_order.phone_number = '012345678901'
+        @item_order.valid?
+        expect(@item_order.errors.full_messages).to include('Phone number Input only number')
+      end
+    
+      it 'tokenが空では登録できない' do
+        @item_order.token = nil
+        @item_order.valid?
+        expect(@item_order.errors.full_messages).to include("Token can't be blank")
+      end
+    end
   end
 end
