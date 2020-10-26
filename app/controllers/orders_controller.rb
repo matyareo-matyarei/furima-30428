8d984621b933c6a546486e258e524d15c9d6bb09
matyareo-matyarei <masa.ahonya.yuka.ahonya@gmail.com>
2020-10-26 16:57:40 +0900

    購入済みの商品の購入ページに遷移しないよう条件を追記

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,7 +3,7 @@ class OrdersController < ApplicationController
   before_action :set_item, only: [:index, :create]
 
   def index
-    redirect_to root_path if current_user.id == @item.user_id
+    redirect_to root_path if current_user.id == @item.user_id || @item.order.present?
     @item_order = ItemOrder.new
   end
 
